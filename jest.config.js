c5b35df343c1bc21587ff026bcca302923ac0ac1
Brian Andrews <sbardian@gmail.com>
2019-03-04 08:52:16 -0700

    Add jest config

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  collectCoverage: true,
+  coverageDirectory: './coverage',
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  testEnvironment: 'jsdom',
+  moduleDirectories: ['node_modules'],
+};
