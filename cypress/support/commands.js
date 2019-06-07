87497e5e249635a796a2dd0dc4d796b655352782
Brian Andrews <sbardian@gmail.com>
2019-06-07 10:18:15 -0600

    chore: Update cypress testing library

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,4 +24,4 @@
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-import 'cypress-testing-library/add-commands';
+import '@testing-library/cypress/add-commands';
