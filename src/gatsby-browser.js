110a1662fabfd49c207ebc7974258d5d83db8307
Brian Andrews <sbardian@gmail.com>
2019-09-05 18:31:20 -0600

    fix(gatsby-node): remove breaking propTypes in gatsby-node

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,19 +1,15 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable import/prefer-default-export */
-import React from 'react';
-import PropTypes from 'prop-types';
-import { BreadcrumbProvider } from './components/BreadcrumbContext';
+/* eslint-disable react/prop-types */
+import React from 'react'
+import { BreadcrumbProvider } from './components/BreadcrumbContext'
 
 export const wrapRootElement = ({ element }, pluginOptions) => {
   if (pluginOptions && pluginOptions.defaultCrumb) {
-    const { defaultCrumb } = pluginOptions;
+    const { defaultCrumb } = pluginOptions
     return (
       <BreadcrumbProvider setHome={defaultCrumb}>{element}</BreadcrumbProvider>
-    );
+    )
   }
-  return <BreadcrumbProvider>{element}</BreadcrumbProvider>;
-};
-
-wrapRootElement.propTypes = {
-  element: PropTypes.node().isRequired,
-};
+  return <BreadcrumbProvider>{element}</BreadcrumbProvider>
+}
