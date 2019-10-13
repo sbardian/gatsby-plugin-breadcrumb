12159d9ea30276e8114f13c184f79b0cb42ab6ac
Brian Andrews <sbardian@gmail.com>
2019-10-13 14:18:00 -0600

    feat(autogen): add hiddenCrumbs and disableCrumbs properties
    allow users to hide and disable links on sections of the breadcrumb based on pathname

--- src/components/auto-gen-crumb.js
+++ src/components/auto-gen-crumb.js
@@ -13,6 +13,8 @@ const AutoGenCrumb = ({
   crumbStyle,
   crumbs: autoGenCrumbs,
   crumbLabel: crumbLabelOverride = null,
+  disableLinks,
+  hiddenCrumbs,
   ...rest
 }) => {
   const { useClassNames } = React.useContext(OptionsContext)
@@ -21,6 +23,9 @@ const AutoGenCrumb = ({
     <div>
       <span>{title}</span>
       {autoGenCrumbs.map((c, i) => {
+        if (hiddenCrumbs.includes(c.pathname)) {
+          return null
+        }
         return (
           <div
             className="breadcrumb"
@@ -29,36 +34,58 @@ const AutoGenCrumb = ({
             }
             key={i}
           >
-            <Link
-              to={c.pathname}
-              style={
-                useClassNames
-                  ? null
-                  : {
-                      textDecoration: 'none',
-                      fontSize: '16pt',
-                      color: '#e1e1e1',
-                      ...crumbStyle,
-                    }
-              }
-              activeStyle={
-                useClassNames
-                  ? null
-                  : {
-                      color: 'white',
-                      ...crumbActiveStyle,
-                    }
-              }
-              className="breadcrumb__link"
-              activeClassName={
-                useClassNames ? 'breadcrumb__link__active' : null
-              }
-              {...rest}
-            >
-              {crumbLabelOverride && i === autoGenCrumbs.length - 1
-                ? crumbLabelOverride
-                : c.crumbLabel}
-            </Link>
+            {!disableLinks.includes(c.pathname) && (
+              <Link
+                to={c.pathname}
+                style={
+                  useClassNames
+                    ? null
+                    : {
+                        textDecoration: 'none',
+                        fontSize: '16pt',
+                        color: '#e1e1e1',
+                        ...crumbStyle,
+                      }
+                }
+                activeStyle={
+                  useClassNames
+                    ? null
+                    : {
+                        color: 'white',
+                        ...crumbActiveStyle,
+                      }
+                }
+                className="breadcrumb__link"
+                activeClassName={
+                  useClassNames ? 'breadcrumb__link__active' : null
+                }
+                {...rest}
+              >
+                {crumbLabelOverride && i === autoGenCrumbs.length - 1
+                  ? crumbLabelOverride
+                  : c.crumbLabel}
+              </Link>
+            )}
+            {disableLinks.includes(c.pathname) && (
+              <span
+                style={
+                  useClassNames
+                    ? null
+                    : {
+                        textDecoration: 'none',
+                        fontSize: '16pt',
+                        color: '#e1e1e1',
+                        ...crumbStyle,
+                      }
+                }
+                className="breadcrumb__link__disabled"
+                {...rest}
+              >
+                {crumbLabelOverride && i === autoGenCrumbs.length - 1
+                  ? crumbLabelOverride
+                  : c.crumbLabel}
+              </span>
+            )}
             <span
               className="breadcrumb__separator"
               style={useClassNames ? null : { fontSize: '16pt', ...crumbStyle }}
@@ -79,6 +106,8 @@ AutoGenCrumb.defaultProps = {
   crumbStyle: {},
   crumbActiveStyle: {},
   crumbLabel: null,
+  disableLinks: [],
+  hiddenCrumbs: [],
 }
 
 AutoGenCrumb.propTypes = {
@@ -94,6 +123,8 @@ AutoGenCrumb.propTypes = {
     }),
   ).isRequired,
   crumbLabel: Proptypes.string,
+  disableLinks: Proptypes.arrayOf,
+  hiddenCrumbs: Proptypes.arrayOf,
 }
 
 export default AutoGenCrumb
