7e6720b28363b9cb9c937127b4b8aff334a9d376
Brian Andrews <sbardian@gmail.com>
2019-11-11 20:46:30 -0700

    test: additional click tracking test

--- src/components/Breadcrumb.test.js
+++ src/components/Breadcrumb.test.js
@@ -1,5 +1,10 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import {
+  render,
+  cleanup,
+  // fireEvent,
+  // waitForElementToBeRemoved,
+} from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Breadcrumb from './Breadcrumb'
 import { OptionsProvider } from './options-context'
@@ -24,19 +29,31 @@ const props = {
   crumbSeparator: ' - ',
 }
 
+const defaultCrumb = {
+  location: {
+    pathname: '/',
+  },
+  crumbLabel: 'HomeCustom',
+  crumbSeparator: ' / ',
+  crumbStyle: { color: '#666' },
+  crumbActiveStyle: { color: 'orange' },
+}
+
 const useAutoGen = false
 const useClassNames = false
 const usePathPrefix = null
 
+afterEach(cleanup)
+
 describe('Breadcrumb Click Tracking', () => {
-  it('Should render click tracking breadcrumb with default home', () => {
-    const { debug, container } = render(
+  it('Should render click tracking breadcrumb with default crumb', () => {
+    const { getByText } = render(
       <OptionsProvider
         useAutoGen={useAutoGen}
         useClassNames={useClassNames}
         usePathPrefix={usePathPrefix}
       >
-        <BreadcrumbProvider>
+        <BreadcrumbProvider defaultCrumb={defaultCrumb}>
           <Breadcrumb
             title={props.title}
             location={props.location}
@@ -46,7 +63,50 @@ describe('Breadcrumb Click Tracking', () => {
         </BreadcrumbProvider>
       </OptionsProvider>,
     )
-    debug()
-    expect(container).toBeTruthy()
+    expect(getByText('HomeCustom')).toBeTruthy()
+    expect(getByText('testLabel')).toBeTruthy()
   })
+  it('Should Render click tracking breadcrumb without default crumb', () => {
+    const { getByText } = render(
+      <OptionsProvider
+        useAutoGen={useAutoGen}
+        useClassNames={useClassNames}
+        usePathPrefix={usePathPrefix}
+      >
+        <BreadcrumbProvider defaultCrumb={null}>
+          <Breadcrumb
+            title={props.title}
+            location={props.location}
+            crumbLabel={props.crumbLabel}
+            crumbSeparator={props.crumbSeparator}
+          />
+        </BreadcrumbProvider>
+      </OptionsProvider>,
+    )
+    expect(getByText('testLabel')).toBeTruthy()
+  })
+  // it('Should render click tracking breadcrumb with default crumb and click home removing testLabel', async () => {
+  //   const { getByText, queryByText } = render(
+  //     <OptionsProvider
+  //       useAutoGen={useAutoGen}
+  //       useClassNames={useClassNames}
+  //       usePathPrefix={usePathPrefix}
+  //     >
+  //       <BreadcrumbProvider defaultCrumb={defaultCrumb}>
+  //         <Breadcrumb
+  //           title={props.title}
+  //           location={props.location}
+  //           crumbLabel={props.crumbLabel}
+  //           crumbSeparator={props.crumbSeparator}
+  //         />
+  //       </BreadcrumbProvider>
+  //     </OptionsProvider>,
+  //   )
+  //   const homeLink = getByText('HomeCustom')
+  //   const testLink = getByText('testLabel')
+  //   expect(homeLink).toBeTruthy()
+  //   expect(testLink).toBeTruthy()
+  //   fireEvent.click(homeLink)
+  //   expect(queryByText('testLabel')).toBeNull()
+  // })
 })
