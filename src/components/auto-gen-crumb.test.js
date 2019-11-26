e59db8b0b0bcdf5dd9777609e9fe8a12670b1da8
Brian Andrews <sbardian@gmail.com>
2019-11-26 16:01:04 -0700

    test(auto-gen-crumb): add autogen tests

---
+++ src/components/auto-gen-crumb.test.js
@@ -0,0 +1,104 @@
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Breadcrumb from './Breadcrumb'
+import { OptionsProvider } from './options-context'
+import { BreadcrumbProvider } from './BreadcrumbContext'
+
+const props = {
+  breadcrumb: {
+    crumbs: [
+      {
+        crumbLabel: 'Home',
+        pathname: '/',
+      },
+      {
+        crumbLabel: 'long',
+        pathname: '/long',
+      },
+      {
+        crumbLabel: 'test',
+        pathname: '/long/test',
+      },
+    ],
+    location: '/long/test',
+  },
+  crumbLabel: 'testLabel',
+  crumbSeparator: ' - ',
+}
+
+const useAutoGen = true
+const useClassNames = false
+const usePathPrefix = null
+
+afterEach(cleanup)
+
+describe('Breadcrumb Click Tracking', () => {
+  it('Should Render autogen breadcrumb', () => {
+    const { getByText } = render(
+      <OptionsProvider
+        useAutoGen={useAutoGen}
+        useClassNames={useClassNames}
+        usePathPrefix={usePathPrefix}
+      >
+        <BreadcrumbProvider defaultCrumb={null}>
+          <Breadcrumb
+            title={props.title}
+            crumbs={props.breadcrumb.crumbs}
+            crumbLabel={props.crumbLabel}
+            crumbSeparator={props.crumbSeparator}
+          />
+        </BreadcrumbProvider>
+      </OptionsProvider>,
+    )
+    expect(getByText('Home')).toBeTruthy()
+    expect(getByText('long')).toBeTruthy()
+    expect(getByText('testLabel')).toBeTruthy()
+  })
+  it('Should render autogen breadcrumb, with disabled long crumb', () => {
+    const { container, getByText } = render(
+      <OptionsProvider
+        useAutoGen={useAutoGen}
+        useClassNames={useClassNames}
+        usePathPrefix={usePathPrefix}
+      >
+        <BreadcrumbProvider defaultCrumb={null}>
+          <Breadcrumb
+            title={props.title}
+            crumbs={props.breadcrumb.crumbs}
+            crumbLabel={props.crumbLabel}
+            crumbSeparator={props.crumbSeparator}
+            disableLinks={['/long']}
+          />
+        </BreadcrumbProvider>
+      </OptionsProvider>,
+    )
+    expect(getByText('Home')).toBeTruthy()
+    expect(getByText('long')).toBeTruthy()
+    expect(getByText('testLabel')).toBeTruthy()
+    expect(container.querySelectorAll('a')).toHaveLength(2)
+  })
+  it('Should render autogen breadcrumb, with long crumb hidden', () => {
+    const { container, getByText, queryByText } = render(
+      <OptionsProvider
+        useAutoGen={useAutoGen}
+        useClassNames={useClassNames}
+        usePathPrefix={usePathPrefix}
+      >
+        <BreadcrumbProvider defaultCrumb={null}>
+          <Breadcrumb
+            title={props.title}
+            crumbs={props.breadcrumb.crumbs}
+            crumbLabel={props.crumbLabel}
+            crumbSeparator={props.crumbSeparator}
+            hiddenCrumbs={['/long']}
+          />
+        </BreadcrumbProvider>
+      </OptionsProvider>,
+    )
+    expect(getByText('Home')).toBeTruthy()
+    expect(queryByText('long')).toBeNull()
+    expect(getByText('testLabel')).toBeTruthy()
+    expect(container.querySelectorAll('a')).toHaveLength(2)
+  })
+})
