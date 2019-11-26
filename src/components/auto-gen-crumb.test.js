3eba40f4ef4e23660a0fcba00f180a1254fda049
Brian Andrews <sbardian@gmail.com>
2019-11-26 16:59:48 -0700

    test(auto-gen-crumb.test): add auto gen tests
    test added for useClassNames

--- src/components/auto-gen-crumb.test.js
+++ src/components/auto-gen-crumb.test.js
@@ -101,4 +101,30 @@ describe('Breadcrumb Click Tracking', () => {
     expect(getByText('testLabel')).toBeTruthy()
     expect(container.querySelectorAll('a')).toHaveLength(2)
   })
+
+  it('Should Render autogen breadcrumb using class names', () => {
+    const { container, getByText } = render(
+      <OptionsProvider
+        useAutoGen={useAutoGen}
+        /* eslint-disable react/jsx-boolean-value */
+        useClassNames={true}
+        usePathPrefix={usePathPrefix}
+      >
+        <BreadcrumbProvider defaultCrumb={null}>
+          <Breadcrumb
+            title={props.title}
+            crumbs={props.breadcrumb.crumbs}
+            crumbLabel={props.crumbLabel}
+            crumbSeparator={props.crumbSeparator}
+          />
+        </BreadcrumbProvider>
+      </OptionsProvider>,
+    )
+    expect(getByText('Home')).toBeTruthy()
+    expect(getByText('long')).toBeTruthy()
+    expect(getByText('testLabel')).toBeTruthy()
+    container
+      .querySelectorAll('a')
+      .forEach(a => expect(a.getAttribute('style')).toBeNull())
+  })
 })
